357db8eaa1f7e636d148f992d0de87e8bb232fa5
Alejandro Duarte <alejandro.duarte@exozet.com>
2016-12-23 01:17:31 +0100

    Added app.js

--- build/js/main.js
+++ build/js/main.js
@@ -2074,7 +2074,7 @@ var requirejs, require, define;
     //Set up with config info.
     req(cfg);
 }(this));
-;require({"urlArgs": "cb=1482451637240"});
+;require({"urlArgs": "cb=1482452180570"});
 
 requirejs({
 	"baseUrl": "./js/",
