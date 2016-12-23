6cc2ee9c9fa916122f2d2919c8d5d387cb75d58a
Alejandro Duarte <alejandro.duarte@exozet.com>
2016-12-23 10:58:07 +0100

    Minified stage-bg-img.

--- build/js/main.js
+++ build/js/main.js
@@ -2074,7 +2074,7 @@ var requirejs, require, define;
     //Set up with config info.
     req(cfg);
 }(this));
-;require({"urlArgs": "cb=1482452521784"});
+;require({"urlArgs": "cb=1482486816274"});
 
 requirejs({
 	"baseUrl": "./js/",
