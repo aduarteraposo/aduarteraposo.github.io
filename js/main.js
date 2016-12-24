75d0b6fed3dab1ec72d255f52fbd1462af435d66
Alejandro Duarte <alejandro.duarte@exozet.com>
2016-12-24 01:51:56 +0100

    Changed redirect in form.

--- js/main.js
+++ js/main.js
@@ -2074,7 +2074,7 @@ var requirejs, require, define;
     //Set up with config info.
     req(cfg);
 }(this));
-;require({"urlArgs": "cb=1482540013010"});
+;require({"urlArgs": "cb=1482540666701"});
 
 requirejs({
 	"baseUrl": "./js/",
